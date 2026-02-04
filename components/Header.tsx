478e3a8deb99cbebf87f47495a8d468ca5952df3
Oktay <oktay@yatko.dev>
2026-02-04 19:58:34 +0100

    Add MCSR TV link in header menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -72,6 +72,29 @@ function Menu() {
           Leaderboard
         </a>
       </li>
+      <li>
+        <a href="https://mcsr.tv" target="_blank">
+          <img
+            className="size-5"
+            src="https://mcsr.tv/favicon-32x32.png"
+            alt="MCSR TV logo"
+          />
+          <span className="flex items-start gap-1">
+            <span className="font-minecraft">MCSR TV</span>
+            <svg
+              className="size-4.5"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M21 11V3h-8v2h4v2h-2v2h-2v2h-2v2H9v2h2v-2h2v-2h2V9h2V7h2v4h2zM11 5H3v16h16v-8h-2v6H5V7h6V5z"
+                fill="currentColor"
+              />
+            </svg>
+          </span>
+        </a>
+      </li>
     </ul>
   );
 }
